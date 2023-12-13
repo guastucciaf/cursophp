461672d26cd43044dc5d1d56a3492dda184b0514
Afonso Guastucci <guastucciaf@gmail.com>
2023-12-13 17:41:49 -0300

    declarado o main

--- desafios/des002/index.php
+++ desafios/des002/index.php
@@ -7,13 +7,15 @@
     <title>Gerador de numeros aleatorios</title>
 </head>
 <body>
-    <h1>Trabalhando com numeros aleatorios</h1>
-    <p>Gerando um numero aleatorio entre 0 e 100...</p>
-    <?php 
-    $num = mt_rand(0,100);
-    echo "<p>O número sorteado foi $num</p> <br>";
-    
-    ?>
-    <button id="atualizar" onclick="javascript:location.reload()">&#x1F503 Sortear novo número</button>
+    <main>
+        <h1>Trabalhando com numeros aleatorios</h1>
+        <p>Gerando um numero aleatorio entre 0 e 100...</p>
+        <?php
+        $num = mt_rand(0,100);
+        echo "<p>O número sorteado foi $num</p> <br>";
+        
+        ?>
+        <button id="atualizar" onclick="javascript:location.reload()">&#x1F503 Sortear novo número</button>
+    </main>
 </body>
 </html>
