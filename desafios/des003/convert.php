c7e9c2f74ef23149a7c2c31cdfc096ed68ddd419
Afonso Guastucci <guastucciaf@gmail.com>
2023-12-13 21:25:28 -0300

    desafio 3
    inserido com sucesso

---
+++ desafios/des003/convert.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Conversor de moeda</title>
+</head>
+<body>
+    <main>
+        <h1>Conversor de moeda v1.0</h1>
+        <?php 
+        $reais = $_GET["valor"];
+        $cota = 4.92;
+        $dolar = $reais/$cota;
+        echo "Seus R$$reais equivalem a $$dolar \n";
+        echo "<p><strong>Cotação fixa de R$$cota</strong> inserida diretamente no código.</p>";
+        ?>
+        <p>
+            <a href="./index.html">Clique aqui para nova conversão</a>
+        </p>
+        
+    </main>
+</body>
+</html>
