54116dba230fceff2c74233b391e20d75d9a7d55
Afonso Guastucci <guastucciaf@gmail.com>
2024-03-17 04:00:48 -0300

    Coalescência Nula
    Added comentário acerca do operador de coalêscencia nula, que havia passado batido

--- exercicios/ex004/cad.php
+++ exercicios/ex004/cad.php
@@ -13,7 +13,7 @@
     <main>
         <?php 
             // var_dump($_REQUEST); // _GET, _POST, _COOKIE
-            $nome = $_GET["nome"] ?? "sem nome";
+            $nome = $_GET["nome"] ?? "sem nome"; // ?? - operador de coalescência nula, funciona a partir do PHP 7
             $sobrenome = $_GET["sobrenome"] ?? "desconhecido";
             echo "<p>É um prazer te conhecer, <strong>$nome $sobrenome</strong>! Este é o meu site!";
 
