293a065ff05a817b1cde22a1c5980fb1f49354e4
Afonso Guastucci <guastucciaf@gmail.com>
2024-04-03 08:27:42 -0300

    desafio 011
    Concluido. Destaque para o elemento HTML Range e sua configuração por meio de um comando JS

--- desafios/des011/index.php
+++ desafios/des011/index.php
@@ -8,26 +8,36 @@
 </head>
 <body>
     <?php 
-    $price = $_GET ['price'];
-    $adjustment = $_GET ['adjustment'];
+    $price = $_GET ['price'] ?? 0;
+    $adjustment = $_GET ['adjustment'] ?? 0;
     $newPrice = ($price + (($price * $adjustment)/100));
+    $padrao = numfmt_create ("pt-BR", NumberFormatter::CURRENCY);
     ?>
+  
   <main>
     <h1>Reajustador de preços</h1>
     <form action="<?=$_SERVER ['PHP_SELF']?>" method="get">
         <label for="price">Preço do produto (R$)</label>
         <input type="number" name="price" id="price" min="0.10" step="0.01" value="<?=$price?>">
-        <label for="adjustment">Qual será o percetual de reajuste? (<strong><span id="p">?%</span></strong>)<?=$adjustment?></label>
-        <input type="range" name="adjustment" id="adjustment" min="-1000" max="1000" step="1">
+        <label for="adjustment">Qual será o percentual de reajuste? (<strong><span id="p">?</span>%</strong>)</label>
+        <input type="range" name="adjustment" id="adjustment" min="-100" max="100" step="1" oninput="mudaValor()" value="<?=$adjustment?>">
         <input type="submit" value="Reajustar">
 
 
     </form>
   </main>
 
-  <section>
+    <section>
     <h2>Resultado do Reajuste</h2>
-    <p>O produto que custava R$[??], com <strong>[??] de aumento</strong> vai passar a custar R$[??] a partir de agora</p>
-  </section>
+    <p>O produto que custava <?=numfmt_format_currency($padrao,$price,"BRL")?>, com reajuste de <strong><?=$adjustment?>%</strong> vai passar a custar <strong><?=numfmt_format_currency($padrao,$newPrice,"BRL")?></strong> a partir de agora.</p>
+    </section>
+    <script>
+        //Declarações automaticas
+        mudaValor();
+
+        function mudaValor() {
+            p.innerText = adjustment.value;
+        }
+    </script>
 </body>
 </html>
