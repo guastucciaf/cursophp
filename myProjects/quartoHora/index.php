19df6ca34181ef6d1c64b354a52e272f2fc52dd3
Afonso Guastucci <guastucciaf@gmail.com>
2024-04-06 06:17:51 -0300

    projeto quarto de hora
    tentando incrementar a soma de cada turno, na hora inicial do turno

--- myProjects/quartoHora/index.php
+++ myProjects/quartoHora/index.php
@@ -11,21 +11,21 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     
     <?php 
-    $qtdsetor = $_GET ['qtdsetor'] ?? 1;
-    $horaIn = $_GET ['horaIn'];
-    $horaFn = $_GET ['horaFn'];
+    $qtdsetor = $_GET ["qtdsetor"];
+    $datahoraIn = $_GET ["datahoraIn"];
+    $datahoraFn = $_GET ["datahoraFn"];
         
     ?>
     
     <main>
         <h1>Quartos de hora</h1>
         <form action="<?=$_SERVER ['PHP_SELF']?>" method="get">
-        <label for="second">Quantos turnos no setor? (<span id="counter"></span>)</label>
+        <label for="qtdsetor">Quantos turnos no setor? (<span id="counter"> </span>)</label>
         <input type="range" name="qtdsetor" id="qtdsetor" min="1" max="10" step="1" oninput="mudaValor()" value="<?=$qtdsetor?>">
         <label for="horaIn">Horário Inicial</label>
-        <input type="datetime-local" name="horaIn" id="horaIn">
+        <input type="datetime-local" name="datahoraIn" id="horaIn" value="<?=$datahoraIn?>">
         <label for="horaFn">Horário Final</label>
-        <input type="datetime-local" name="horaFn" id="horaFn">
+        <input type="datetime-local" name="datahoraFn" id="horaFn" value="<?=$datahoraFn?>">
         <input type="submit" value="Calcular">
         
 
@@ -33,12 +33,40 @@
     </main>
 
     <section>
-        <?php 
-        $horaTotal = $horaIn->diff($horaFn);
-
-        ?>
+        <pre>
+            <?php
+            $horaIn = strtotime ($datahoraIn); //converte a string de data em segundos
+            $horaFn = strtotime ($datahoraFn);
+            $intervaloMinutos = ($horaFn - $horaIn) / 60;
+            $minutosCada = ($intervaloMinutos / $qtdsetor);
+            
+            ?>
+        </pre>
+    
     <h2>Resultado</h2>
-    <p>Tempo de cada turno é <?=$horaTotal?></p>
+    <p>O tempo total de serviço é de <strong><?=$intervaloMinutos?> minutos</strong>.</p>
+    <p>Tempo de cada turno é de <strong><?=$minutosCada?> minutos</strong>.</p>
+    
+    <?php 
+    
+
+    function segundosEmTempo ($horaIn) {
+        $hora = ($horaIn / 3600);
+        $minuto = ($horaIn / 60);
+
+        return sprintf("%d:%02", $hora, $minuto);
+
+    }
+    
+    echo "<p>Segue a divisão dos QUARTOS DE HORA: <br>";
+    $counter = 1;
+    while ($counter <= $qtdsetor) :
+        echo $counter."º turno - ".segundosEmTempo($horaIn)."<br>";
+        $counter++;
+        
+    endwhile;
+    ?>
+    
         
     </section>
     <script>
