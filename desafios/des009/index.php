527c8696bfca45d32b3cfef4cba035a6ebea0818
Afonso Guastucci <guastucciaf@gmail.com>
2024-05-01 08:36:01 -0300

    Update index.php

--- desafios/des009/index.php
+++ desafios/des009/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafio PHP 9</title>
+    <title>Desafio PHP 9 - Médias Aritméticas</title>
     <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
